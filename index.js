cb0057c4c9173fbe2107a7dcb49463664c351b1f
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-15 20:46:09 +0700

    show online user

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('home'));
 server.listen(3000, () => console.log('Server started!'));
-
-io.on('connection', socket => console.log(socket.id));
-
 reload(app);
+
+//arrUsername
+const arrUsername = [];
+
+io.on('connection', socket => {
+    socket.on('CLIENT_SIGN_UP', username => {
+        const isEsixt = arrUsername.indexOf(username) > -1;
+        if (isEsixt) return socket.emit('PICK_ANOTHER_USERNAME');
+        arrUsername.push(username);
+        socket.emit('SIGN_UP_SUCCESSFULLY', arrUsername);
+    });
+});
--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
 const socket = io();
 
 $('#div-chat').hide();
+
+socket.on('PICK_ANOTHER_USERNAME', () => alert('Choose another username'));
+
+socket.on('SIGN_UP_SUCCESSFULLY', arrUsername => {
+    $('#div-chat').show();
+    $('#div-dang-ky').hide();
+
+    arrUsername.forEach(username => $('#ul-username').append(`<li>${username}</li>`));
+});
+
+$('#btn-dang-ky').click(() => {
+    const username = $('#txt-username').val();
+    socket.emit('CLIENT_SIGN_UP', username);
+});
